5adaa06cc057095c80df7d3c3f16695dc4cd08f3
dor.koren <dor.koren@post.idc.ac.il>
2018-11-12 14:06:31 +0200

    1. Add a new toolboxes. 2. log in to facebook

--- DesktopFacebook/Form1.Designer.cs
+++ DesktopFacebook/Form1.Designer.cs
@@ -31,7 +31,11 @@
             this.buttonLogin = new System.Windows.Forms.Button();
             this.pictureBoxUser = new System.Windows.Forms.PictureBox();
             this.labelAccountName = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxUser)).BeginInit();
+            this.tabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
             // buttonLogin
@@ -44,6 +48,7 @@
             this.buttonLogin.TabIndex = 0;
             this.buttonLogin.Text = "Login";
             this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
             // 
             // pictureBoxUser
             // 
@@ -67,12 +72,43 @@
             this.labelAccountName.TabIndex = 2;
             this.labelAccountName.Text = "Account Name";
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(258, 45);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(530, 393);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.LightGray;
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(522, 364);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Birthday Wish";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.LightGray;
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(522, 364);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Import Shared Friend Photos";
+            // 
             // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.labelAccountName);
             this.Controls.Add(this.pictureBoxUser);
             this.Controls.Add(this.buttonLogin);
@@ -80,6 +116,7 @@
             this.Text = "Desktop Facebook";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxUser)).EndInit();
+            this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -90,6 +127,9 @@
         private System.Windows.Forms.Button buttonLogin;
         private System.Windows.Forms.PictureBox pictureBoxUser;
         private System.Windows.Forms.Label labelAccountName;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
     }
 }
 
--- DesktopFacebook/Form1.cs
+++ DesktopFacebook/Form1.cs
@@ -7,11 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Facebook;
+using FacebookWrapper;
 
 namespace DesktopFacebook
 {
     public partial class mainForm : Form
     {
+        private LoginResult m_LoginResult { get; set; }
+        private FacebookWrapper.ObjectModel.User m_LoggetInUser;
+
+
         public mainForm()
         {
             InitializeComponent();
@@ -20,6 +26,20 @@ namespace DesktopFacebook
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            m_LoginResult = FacebookService.Login(
+                "2121776861417547",
+                "user_birthday",
+                "user_photos");
+
+            m_LoggetInUser = m_LoginResult.LoggedInUser;
+            this.Text = "Logged in as " + m_LoggetInUser.FirstName + " " + m_LoggetInUser.LastName;
+            labelAccountName.Text = m_LoggetInUser.FirstName + " " + m_LoggetInUser.LastName;
+            pictureBoxUser.ImageLocation = m_LoggetInUser.PictureNormalURL;
+            buttonLogin.Text = "Log out";
         }
     }
 }
